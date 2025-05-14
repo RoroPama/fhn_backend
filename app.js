63a9bce9754500b5dbab2c3dc4cbf2e938e83db6
Rolic PAMA <pamarolic017@gmail.com>
2025-05-14 14:33:56 +0100

    [UPDATE]: cors updated 2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
